f3622ccb61d9a2a3b0a66a796e36ea72e7dd2990
A1suluu <baisheva2004@gmail.com>
2022-03-03 20:42:15 +0600

    already finished home page

--- reviro/app.js
+++ reviro/app.js
@@ -1,1 +1,36 @@
 document.addEventListener("DOMContentLoaded", main)
+
+
+
+
+
+
+
+
+
+
+
+
+// assignments 
+
+// 1. create web 2 pages with html and css code 
+// 2. create detail page and page to searching and finding information
+// 3. commit and add most of all changings
+// 4. create some logical js code to fund any information about news.
+// Fetch other site in order to user could find any current info 
+// 5. also fetch detail html with any current news. Like current information about maybe 
+// dollar exchange rate or current info from Ukraine
+// 6. check all my code and site
+
+
+// what are ypu thinkimg about us === saving in localStorage
+// advisor === going to telegrame chat
+// sharing with news with ur friends
+// login that user may to create 
+// Удаление element from id of object
+
+
+// Просмотр детали новости по id 
+//Создание новости
+// Просмотр новости
+// Редактирование новости
