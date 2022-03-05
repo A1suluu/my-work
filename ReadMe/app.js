97cb5212dab1d5905aaf318db25dfb00f6ef51e0
A1suluu <baisheva2004@gmail.com>
2022-03-05 14:31:21 +0600

    search api code

--- ReadMe/app.js
+++ ReadMe/app.js
@@ -36,3 +36,54 @@
 //     }
 //     return t
 // }
+
+
+
+ $(document).bind('pageinit', function(ev) {
+   $('#search').on('keyup', function(e) {
+     if (e.keyCode == 13) {
+
+       // The Base URL
+       var baseUrl = 'https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=';
+
+       // The question from the inputfield
+       var q = $('#search').val();
+
+       // putting the url togehter and append &callback=?
+       var url = baseUrl + q + "&callback=?";
+       console.log(url);
+
+       // Call The API for a JSON
+       $.getJSON(url, function() {
+         console.log("success");
+       }).done(function(data) {
+         console.log("second success");
+
+         console.log(data.responseData.results);
+
+         // create a var for the results and append a header
+         var results = '<li data-role="list-divider">Results</li>';
+
+$.each(data.responseData.results, function(index, item) {
+
+    results += '<li>';
+    results += item.title;
+    results += '</li>';
+  });
+
+
+
+  // clear the results . append the results .refresh the listview
+  $('#results').empty().append(results).listview('refresh');
+
+}).fail(function() {
+  console.log("error");
+}).always(function() {
+  console.log("always");
+});
+
+
+
+}
+});
+});
