2cbb09dddad45aba6abd46179f65efddb004e1d7
A1suluu <baisheva2004@gmail.com>
2022-03-06 18:25:38 +0600

    add advrt files

--- reviro/searchNews/search.js
+++ reviro/searchNews/search.js
@@ -16,9 +16,10 @@ function retrieve (e) {
     newList.innerHTML = " "
     e.preventDefault()
 
+    
     const apiKey = "beca3e07eb544a13992c3bd77e7bdaab"
     let topic = input.value;
-
+    
     let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
     
     fetch(url).then((res) => {
@@ -29,7 +30,6 @@ function retrieve (e) {
             let li = document.createElement("li")
             let a = document.createElement("a")
             a.setAttribute("href", article.url)
-            a.setAttribute("target", "_blank")
             a.textContent = article.title
             li.appendChild(a)
             newList.appendChild(li)
@@ -39,8 +39,13 @@ function retrieve (e) {
             // conteiner.style.height = `${240}px` + newListWidth
             // conteiner.style.height = newListWidth
             // console.log(newListWidth)
-           conteiner.style.height = `${100}%`
-
+            conteiner.style.height = `${100}%`
+            
+            // let img = document.createElement("img")
+            // img.setAttribute("scr", article.urlToImage)
+            // li.appendChild(img)
+    
+           
         })
     }).catch((error) => {
         console.log(error)
