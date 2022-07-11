9458f1d2bcf45c0b31c9acd755ccc91f63ba873e
A1suluu <baisheva2004@gmail.com>
2022-07-11 17:56:26 +0600

    add new file artgrame and changes in this art project

---
+++ artgrame/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from './comps/Title';
+import UploadForm from './comps/UpLoadForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+      <UploadForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ artgrame/src/firebase/config.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import 'firebase/firestore'
+import 'firebase/storage'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUBEVh3-MNT2B_ZuObNzlBNM9goYWc3mw",
+  authDomain: "firegram-340a3.firebaseapp.com",
+  projectId: "firegram-340a3",
+  storageBucket: "firegram-340a3.appspot.com",
+  messagingSenderId: "872618895810",
+  appId: "1:872618895810:web:04fac1f54fd3ef80dbce5b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage()
+const projectFireStore = firebase.fireStore()
+
+export {projectStorage, projectFireStore}
